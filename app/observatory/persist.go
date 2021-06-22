0cb3479993d47e556bb156b1690da4ab64465ba7
世界 <i@sekai.icu>
2021-06-22 19:20:27 +0800

    Add hook to listen observatory status

--- app/observatory/persist.go
+++ app/observatory/persist.go
@@ -1,10 +1,10 @@
 package observatory
 
-func (o *Observer) UpdateStatus(outbound string, result *OutboundStatus) {
+func (o *Observer) UpdateStatus(result *OutboundStatus) {
 	o.statusLock.Lock()
 	defer o.statusLock.Unlock()
 
-	if location := o.findStatusLocationLockHolderOnly(outbound); location != -1 {
+	if location := o.findStatusLocationLockHolderOnly(result.OutboundTag); location != -1 {
 		o.status[location] = result
 	} else {
 		o.status = append(o.status, result)
