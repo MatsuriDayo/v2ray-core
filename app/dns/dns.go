5d0e86825fc9cccced5e5dc25029572f3ca458c6
世界 <i@sekai.icu>
2021-09-13 10:01:20 +0800

    Add concurrent option for dns server

--- app/dns/dns.go
+++ app/dns/dns.go
@@ -9,9 +9,6 @@ package dns
 import (
 	"context"
 	"fmt"
-	"strings"
-	"sync"
-
 	"github.com/v2fly/v2ray-core/v4/app/router"
 	"github.com/v2fly/v2ray-core/v4/common"
 	"github.com/v2fly/v2ray-core/v4/common/errors"
@@ -20,6 +17,8 @@ import (
 	"github.com/v2fly/v2ray-core/v4/common/strmatcher"
 	"github.com/v2fly/v2ray-core/v4/features"
 	"github.com/v2fly/v2ray-core/v4/features/dns"
+	"strings"
+	"sync"
 )
 
 // DNS is a DNS rely server.
@@ -195,7 +194,7 @@ func (s *DNS) LookupIPv6(domain string) ([]net.IP, error) {
 	return s.lookupIPInternal(domain, o)
 }
 
-func (s *DNS) lookupIPInternal(domain string, option dns.IPOption) ([]net.IP, error) {
+func (s *DNS) lookupIPInternal(domain string, option dns.IPOption) (result []net.IP, reterr error) {
 	if domain == "" {
 		return nil, newError("empty domain name")
 	}
@@ -218,24 +217,74 @@ func (s *DNS) lookupIPInternal(domain string, option dns.IPOption) ([]net.IP, er
 	}
 
 	// Name servers lookup
-	errs := []error{}
+	var errs []error
 	ctx := session.ContextWithInbound(s.ctx, &session.Inbound{Tag: s.tag})
+	done, cancel := context.WithCancel(context.Background())
+	var access sync.Mutex
+	result = nil
+	remain := 0
 	for _, client := range s.sortClients(domain) {
-		if !option.FakeEnable && strings.EqualFold(client.Name(), "FakeDNS") {
-			newError("skip DNS resolution for domain ", domain, " at server ", client.Name()).AtDebug().WriteToLog()
-			continue
+		client := client
+		access.Lock()
+		remain++
+		access.Unlock()
+
+		query := func() (ret bool) {
+			if !option.FakeEnable && strings.EqualFold(client.Name(), "FakeDNS") {
+				newError("skip DNS resolution for domain ", domain, " at server ", client.Name()).AtDebug().WriteToLog()
+				return
+			}
+			ips, err := client.QueryIP(ctx, domain, option, s.disableCache)
+			access.Lock()
+			defer access.Unlock()
+
+			select {
+			case <-done.Done():
+				return
+			default:
+			}
+
+			remain--
+			if len(ips) > 0 {
+				result = ips
+				ret = true
+			} else if err != nil {
+				newError("failed to lookup ip for domain ", domain, " at server ", client.Name()).Base(err).WriteToLog()
+				errs = append(errs, err)
+				if err != context.Canceled && err != context.DeadlineExceeded && err != errExpectedIPNonMatch {
+					reterr = err
+					ret = true
+				}
+			}
+
+			if remain == 0 {
+				ret = true
+			}
+
+			if ret {
+				cancel()
+			}
+			return
 		}
-		ips, err := client.QueryIP(ctx, domain, option, s.disableCache)
-		if len(ips) > 0 {
-			return ips, nil
+
+		if client.concurrent {
+			go query()
+		} else {
+			if query() {
+				return result, reterr
+			}
 		}
-		if err != nil {
-			newError("failed to lookup ip for domain ", domain, " at server ", client.Name()).Base(err).WriteToLog()
-			errs = append(errs, err)
-		}
-		if err != context.Canceled && err != context.DeadlineExceeded && err != errExpectedIPNonMatch {
-			return nil, err
-		}
+	}
+
+	select {
+	case <-done.Done():
+	case <-ctx.Done():
+	}
+
+	cancel()
+
+	if result != nil || reterr != nil {
+		return result, reterr
 	}
 
 	return nil, newError("returning nil for domain ", domain).Base(errors.Combine(errs...))
