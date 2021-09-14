f7de86b0050bb3abf7a75fedb153fd6558c2f6aa
世界 <i@sekai.icu>
2021-09-14 12:58:04 +0800

    Add routeOnly sniffing

--- app/dispatcher/default.go
+++ app/dispatcher/default.go
@@ -227,7 +227,11 @@ func (d *DefaultDispatcher) Dispatch(ctx context.Context, destination net.Destin
 				domain := result.Domain()
 				newError("sniffed domain: ", domain).WriteToLog(session.ExportIDToError(ctx))
 				destination.Address = net.ParseAddress(domain)
-				ob.Target = destination
+				if sniffingRequest.RouteOnly && result.Protocol() != "fakedns" {
+					ob.RouteTarget = destination
+				} else {
+					ob.Target = destination
+				}
 			}
 			d.routedDispatch(ctx, outbound, destination)
 		}()
--- infra/conf/cfgcommon/sniffer/sniffer.go
+++ infra/conf/cfgcommon/sniffer/sniffer.go
@@ -13,6 +13,7 @@ type SniffingConfig struct {
 	Enabled      bool                  `json:"enabled"`
 	DestOverride *cfgcommon.StringList `json:"destOverride"`
 	MetadataOnly bool                  `json:"metadataOnly"`
+	RouteOnly    bool                  `json:"routeOnly"`
 }
 
 // Build implements Buildable.
@@ -41,5 +42,6 @@ func (c *SniffingConfig) Build() (*proxyman.SniffingConfig, error) {
 		Enabled:             c.Enabled,
 		DestinationOverride: p,
 		MetadataOnly:        c.MetadataOnly,
+		RouteOnly:           c.RouteOnly,
 	}, nil
 }
